2e62b3bf70db97bf1833d5da6e7afb793d94f9c2
Exequiel Sosa <exequielsosa@gmail.com>
2026-01-02 23:56:35 -0300

    fix: actualizar la fecha de modificación en los metadatos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -181,7 +181,7 @@ export async function generateMetadata(): Promise<Metadata> {
       type: "article",
       locale: locale === "es" ? "es_AR" : "en_US",
       publishedTime: "2025-11-01T00:00:00Z",
-      modifiedTime: "2026-01-01T00:00:00Z",
+      modifiedTime: "2026-01-03T00:00:00Z",
       images: [
         {
           url: "/images/megadeth-megadeth.jpg",
