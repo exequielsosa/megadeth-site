7864b3fa19c4262e1b68a023c2762527a88fbe3d
Exequiel Sosa <exequielsosa@gmail.com>
2026-01-10 10:00:10 -0300

    feat: agregar entrevistas y noticias sobre los cumpleaños de Dirk y Temu, incluyendo nuevas imágenes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -183,7 +183,7 @@ export async function generateMetadata(): Promise<Metadata> {
       type: "article",
       locale: locale === "es" ? "es_AR" : "en_US",
       publishedTime: "2025-11-01T00:00:00Z",
-      modifiedTime: "2026-01-06T00:00:00Z",
+      modifiedTime: "2026-01-10T00:00:00Z",
       images: [
         {
           url: "/images/megadeth-megadeth.jpg",
